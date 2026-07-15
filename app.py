8a53d7145daf0e261751f949111a3ef696fca782
Achyuta41 <Achyutak41@gmail.com>
2026-07-15 20:06:09 +0530

    Fix vercel configurations

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 
 @app.route("/")
 def index():
